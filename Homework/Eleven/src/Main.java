0cc892526390e35ed6b83218698329caa09f9c2e
JolantaDanilova <evangeliade@gmail.com>
2023-02-28 19:59:32 +0100

    HomeworkJava22.02.2023
     Задание 1
    установить JDK 11, 17
    создать два проекта Java, переключить в настройках один на 11, второй на 17
    Задание 2
    В любом из проектов создать несколько разных пакетов
    В каждом пакете создать класс(Java-файл), который при запуске программы выводит сообщения "Привет. Я -  подставить название класса"

---
+++ Homework/Eleven/src/Main.java
@@ -0,0 +1,9 @@
+import de.telrun.Rose;
+
+public class Main {
+    public static void main(String[] args) {
+        Rose welcome = new Rose();
+
+        System.out.println("Привет я - " + welcome.hello);
+    }
+}
---
+++ Homework/Eleven/src/de/telrun/Rose.java
@@ -0,0 +1,5 @@
+package de.telrun;
+
+public class Rose {
+    public String hello = "Роза";
+}
